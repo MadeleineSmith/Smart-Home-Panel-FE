dba92487aef59b7a1d1a6034b9e3044297589cab
Madeleine Smith <madeleine@madeleinesmith.uk>
2022-05-14 17:48:25 +0100

    Initial FE MVP

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    state = {
+        text: '',
+        brightness: 255,
+        colorTemperature: 255,
+    }
+
+    componentDidMount() {
+    }
+
+    updateDevice = async evt => {
+        evt.preventDefault()
+
+        await fetch('/devices/1', {
+            method: 'PATCH',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                "brightness": this.state.brightness,
+                "colorTemperature": this.state.colorTemperature
+            })
+        });
+    }
+
+    handleChange = evt => {
+        this.setState({[evt.target.name]: evt.target.value})
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <h1>Smart Bulbs</h1>
+
+                <form onSubmit={this.updateDevice}>
+                    <label>Brightness</label>
+                    <input
+                        type="number"
+                        name="brightness"
+                        value={this.state.brightness}
+                        onChange={this.handleChange}
+                    />
+
+                    <label>Color temperature</label>
+                    <input
+                        type="number"
+                        name="colorTemperature"
+                        value={this.state.colorTemperature}
+                        onChange={this.handleChange}
+                    />
+
+                    <button type="submit">Adjust settings!</button>
+                </form>
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
